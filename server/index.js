c80edafb1c04d5aee7f6cb453c757c0ed0a409e7
Jacob Ankney <crosswalkastronaut@Jacobs-MacBook-Pro-3.local>
2020-01-21 16:02:06 -0700

    init

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express=require('express')
+const app=express()
+require('dotenv').config()
+
+const port=process.env.PORT ||6968
+
+app.use(express.json());
+app.use(express.static(`${__dirname}/../build`));
+
+
+app.listen(port, ()=>{
+    console.log('listening on ', port)
+})
+
+app.post('/appts', (req, res)=>{
+    console.log(req.body, 'hit')
+    res.sendStatus(200)
+})
+
+
